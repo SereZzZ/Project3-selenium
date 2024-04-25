dae983dd20f172e5793e75bbc691519c94296dd2
Neto sergey test <serez.gavriliv@mail.ru>
2024-04-25 15:24:21 +0300

    add Test

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,62 @@
+import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationTest {
+    private WebDriver driver; //создаем драйвер на основе вебдрайвера
+
+
+    @BeforeAll    //общие настройки для всего класса с тестами
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+
+    }
+
+   /* @BeforeEach  //перед каждым тестом инициализируем в тестах драйвер хром и видим что происходит
+    void setUp(){
+        driver = new ChromeDriver();
+    }
+    */
+
+   @BeforeEach  // перед каждым тестом то же самое только в режиме хедрс для Ci чтобы не было видно интерфейса не видим что происходит
+    void setUP() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+
+   @AfterEach  // после каждого теста выйти и обнулить
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldBeSuccessfulForm() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Полина");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79969236311");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim()); //trim удаляет пробелы с текста с переди и с зади
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id='order-success']")).isDisplayed());
+    }
+}
+
+
+
+
+
---
+++ src/test/java/NegativeTest.java
@@ -0,0 +1,79 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+
+    }
+
+    @BeforeEach
+    void setUP() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldBeSuccessfulForm() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        assertEquals("Поле обязательно для заполнения", text.trim());
+        driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).isDisplayed();
+    }
+
+    @Test
+    void shouldBeFailedIncorrectPhoneInput() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Полина");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7996923631111");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+        driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).isDisplayed();
+    }
+
+    @Test
+    void shouldBeFailedIncorrectNameInput() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Полина1314");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79969236311");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button[type='button']")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText().trim();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+        driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).isDisplayed();
+    }
+
+    @Test
+    void shouldBeFailedIncorrectCheckboxUsed() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Полина");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79969236311");
+        driver.findElement(By.cssSelector("button[type='button']")).click();
+        assertEquals(true, driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid")).isDisplayed());
+    }
+}
